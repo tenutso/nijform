707ff7a76dbf564145c9e4cb71e1e29d71a46c99
Tenutso <tenutso@yahoo.com>
2018-09-25 12:25:32 -0400

    Added sequelize

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  
+  User.createUser('anijmeh', 'capeexe');
   res.send('respond with a resource');
 });
 
